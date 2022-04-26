cc6a2a43d5d503d2dc73b9f0d2e985709ccb0a9c
jyoost <jyoost@gmail.com>
2022-04-25 21:43:53 -0700

    Add param deploy-branch

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,32 +1,29 @@
 #!/bin/bash
 
-# Consume environment variables
 SSH_PRIVATE_KEY=$1
 DOKKU_USER=$2
 DOKKU_HOST=$3
 DOKKU_APP_NAME=$4
 DOKKU_REMOTE_BRANCH=$5
-DEPLOY_BRANCH=$6
-
-echo -n "Deploying application to Dokku host\n"
+GIT_PUSH_FLAGS=$6
+DEPLOY_BRANCH=$7
 
 # Setup the SSH environment
 mkdir -p ~/.ssh
 eval `ssh-agent -s`
-ssh-add - <<< $SSH_PRIVATE_KEY
-ssh-keygen -y -f ~/.ssh/id_rsa > ~/.ssh/id_rsa.pub
+ssh-add - <<< "$SSH_PRIVATE_KEY"
 ssh-keyscan $DOKKU_HOST >> ~/.ssh/known_hosts
-chown 400 ~/.ssh/id_rsa
 
 # Setup the git environment
-REMOTE=$DOKKU_USER@$DOKKU_HOST:$DOKKU_APP_NAME
-cd $GITHUB_WORKSPACE
-git remote add dokku $REMOTE
+git_repo="$DOKKU_USER@$DOKKU_HOST:$DOKKU_APP_NAME"
+cd "$GITHUB_WORKSPACE"
+git remote add deploy "$git_repo"
 
 # Prepare to push to Dokku git repository
-REMOTE_REF=$DEPLOY_BRANCH:refs/heads/$DOKKU_REMOTE_BRANCH:master
-GIT_COMMAND="git push dokku $REMOTE_REF"
+REMOTE_REF="$DEPLOY_BRANCH:$DOKKU_REMOTE_BRANCH"
+
+GIT_COMMAND="git push deploy $REMOTE_REF $GIT_PUSH_FLAGS"
 echo "GIT_COMMAND=$GIT_COMMAND"
 
 # Push to Dokku git repository
-GIT_SSH_COMMAND="ssh -i ~/.ssh/id_rsa -F /dev/null -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no" $GIT_COMMAND
+GIT_SSH_COMMAND="ssh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no" $GIT_COMMAND
